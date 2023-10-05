0d6387cefde3cb5d9878bba8f8101cba8c6b5076
BirukMetaferia <145568670+BirukMetaferia@users.noreply.github.com>
2023-10-05 09:50:06 +0300

    Add files via upload

---
+++ JobSeeker/Profile.php
@@ -0,0 +1,93 @@
+   
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+            
+            <!-- Article -->
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+            <!-- Article -->
+          
+                <h2><a href="#">Welcome <?php echo $_SESSION['Name'];?></a></h2>
+               
+<?php
+$ID=$_SESSION['ID'];
+// Establish Connection with Database
+$con = mysqli_connect("localhost","root","","sourcecodester_jobportaldb");
+// Specify the query to execute
+$sql = "select * from jobseeker_reg where JobSeekId='".$ID."'  ";
+// Execute query
+$result = mysqli_query($con,$sql) or die(mysqli_error($con));
+// Loop through each records 
+$row = mysqli_fetch_array($result)
+?>
+                <table width="100%" border="1" cellspacing="2" cellpadding="2">
+                  <tr>
+                    <td><strong>Name:</strong></td>
+                    <td><?php echo $row['JobSeekerName'];?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Address:</strong></td>
+                    <td><?php echo $row['Address'];?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>City:</strong></td>
+                    <td><?php echo $row['City'];?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Email:</strong></td>
+                    <td><?php echo $row['Email'];?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Mobile:</strong></td>
+                    <td><?php echo $row['Mobile'];?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Highest Qualification:</strong></td>
+                    <td><?php echo $row['Qualification'];?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Gender:</strong></td>
+                    <td><?php echo $row['Gender'];?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Birth Date:</strong></td>
+                    <td><?php echo $row['BirthDate'];?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Resume:</strong></td>
+                    <td><a href="../upload/<?php echo $row['Resume'];?>" target="_blank"><strong>View</strong></a></td>
+                  </tr>
+                  <tr>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                  </tr>
+                </table>
+              <p>&nbsp;</p>
+
+                <p class="btn-more box noprint">&nbsp;</p>
+        
+
+            <?php
+            mysqli_close($con);
+            ?>
+
+            <hr class="noscreen" />
+            
+        
+
+
+
+    
+
+
---
+++ JobSeeker/menu.php
@@ -0,0 +1,20 @@
+<!-- Main menu (tabs) -->
+     <div id="tabs" class="noprint">
+
+            <h3 class="noscreen">Navigation</h3>
+            <ul class="box">
+                <li><a href="index.php">Home<span class="tab-l"></span><span class="tab-r"></span></a></li>
+
+                <li><a href="Education.php">Education<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                <li><a href="SearchJob.php">Search Job<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                <li><a href="Walkin.php">Walkin Interview<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                <li><a href="Feedback.php">Feedback<span class="tab-l"></span><span class="tab-r"></span></a></li>
+
+                <ul class="butt" >
+                  <li class="butt"><a class="butt" id="logoi" onclick="toggl()" href="#"><img class="imgee" src="../design/profile.png"></span></a></li> 
+                  
+                </ul> 
+            </ul>
+
+        <hr class="noscreen" />
+     </div> <!-- /tabs -->
