1f17b332cd22563af29eb21e1e74c0e23627f272
BirukMetaferia <145568670+BirukMetaferia@users.noreply.github.com>
2023-10-05 09:55:45 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,140 @@
+<?php
+session_start();
+if(isset($_SESSION['$UserName'])){
+	header('location:Admin/index.php');
+} 
+if(isset($_SESSION['$UserName_job'])){
+	header('location:JobSeeker/index.php');
+} 
+if(isset($_SESSION['$UserName_emp'])){
+	header('location:Employer/index.php');
+} 
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="cs" />
+    <meta name="robots" content="all,follow" />
+
+    
+    <title>job portal</title>
+    <meta name="description" content="..." />
+    <meta name="keywords" content="..." />
+    
+    <link rel="index" href="./" title="Home" />
+    <link rel="stylesheet" media="screen,projection" type="text/css" href="css/hole.css" />
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" media="screen">
+    <link rel="stylesheet" media="print" type="text/css" href="./css/print.css" />
+    <link rel="stylesheet" media="aural" type="text/css" href="./css/aural.css" />
+    	
+	
+	
+
+    <style type="text/css">
+<!--
+.style1 {
+	color: #000066;
+	font-weight: bold;
+}
+.style2 {
+	font-size: medium;
+	font-weight: bold;
+}
+-->
+    </style>
+</head>
+
+<body id="www-url-cz">
+<!-- Main -->
+<div id="main" class="box">
+<?php 
+include "Header.php"
+?>
+<?php 
+include "menu.php"
+?> 
+
+<!-- Page (2 columns) -->
+    <div id="page" class="box">
+    <div id="page-in" class="box">
+
+        <div id="strip" class="box noprint">
+
+            <!-- RSS feeds -->
+            <hr class="noscreen" />
+
+            <!-- Breadcrumbs -->
+            <p id="breadcrumbs">&nbsp;</p>
+          <hr class="noscreen" />
+            
+        </div> <!-- /strip -->
+
+        <!-- Content -->
+        <div id="content">
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+            
+            <!-- Article -->
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+            <!-- Article -->
+            <div class="article">
+             
+				
+               
+                <div class="homebox1">
+                <p class="homebox1">Welcome to online Job Portal. It provides facility to the Job Seeker to search for various jobs as per his qualification. Here Job Seeker can registered himself on the web portal and create his profile along with his educational information. Job Seeker can search various jobs and apply for the Job.</p>
+                <button class="read"><a class="aread" href="AboutUs.php">Read_more</a></button>
+                </div>
+                <div class="homebox2">
+                 <div class="subdiv">
+              
+                  <img class="slideri" src="design/bk/bi1.jpg">
+                  <img class="slideri" src="design/bk/bi2.jpg">
+                  <img class="slideri" src="design/bk/bi3.jpg">
+                  <img class="slideri" src="design/bk/bi4.jpg">
+                  <img class="slideri" src="design/bk/bi5.jpg">
+                  
+                  
+                  
+                 </div>
+                </div>
+                
+
+              
+          </div> <!-- /article -->
+          
+
+            <hr class="noscreen" />
+            
+        </div> <!-- /content -->
+        
+
+<?php
+include "right.php"
+?>
+
+    </div> <!-- /page-in -->
+    </div> <!-- /page -->
+
+ 
+<?php
+include "footer.php"
+?>
+</div> <!-- /main -->
+
+</body>
+</html>
