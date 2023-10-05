b24a26100659163d3635676da3163bcdcfdededb
BirukMetaferia <145568670+BirukMetaferia@users.noreply.github.com>
2023-10-05 09:37:17 +0300

    Add files via upload

---
+++ Admin/menu.php
@@ -0,0 +1,20 @@
+<!-- Main menu (tabs) -->
+     <div id="tabs" class="noprint">
+
+            <h3 class="noscreen">Navigation</h3>
+            <ul class="box">
+                <li><a href="index.php">Home<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                <li><a href="User.php">User<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                <li><a href="ManageJob.php">Manage JobSeeker<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                <li><a href="ManageEmployer.php">Manage Employer<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                <li><a href="News.php">News<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                <li><a href="Feedback.php">Feedback<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                
+                <ul class="butt" >
+                  <li class="butt"><a class="butt" id="logoi" onclick="toggl()" href="#"><img class="imgee" src="../design/profile.png"></span></a></li> 
+                  
+                </ul> 
+            </ul>
+
+        <hr class="noscreen" />
+     </div> <!-- /tabs -->
---
+++ Admin/right.php
@@ -0,0 +1,33 @@
+
+
+</style>
+
+
+<div id="logod" class="profiles">
+
+    
+    <li ><a class="logouts" href="session_destroy.php">Logout<span class="tab-l"></span><span class="tab-r"></span></a></li>
+</div>
+
+<script type="text/javascript">
+       
+          function toggl(){
+            var div=document.getElementById("logod")
+            var btn=document.getElementById("logoi")
+
+
+            if (div.style.display === "none"){
+              div.style.display = "block";
+              btn.style.marginRight=30;
+
+              
+            }
+            else{
+              div.style.display = "none";
+              
+            }
+
+          }
+          
+
+        </script>
