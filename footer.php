cfa7620a85082bce88a5096c5d702206cd7060c2
BirukMetaferia <145568670+BirukMetaferia@users.noreply.github.com>
2023-10-05 09:53:45 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,89 @@
+<link rel="stylesheet" type="text/css" href="css/footer.css" media="screen">
+<footer class="footer-wrapper">
+			
+            <div class="main-footer">
+            
+                <div class="container">
+                
+                    <div class="row">
+                    
+                        <div class="col-sm-12 col-md-9">
+                        
+                            <div class="row">
+                            
+                                <div class="col-sm-6 col-md-4">
+                                
+                                <div class="footer-about-us">
+                                        <h5 class="footer-title">About job portal</h5>
+                                        <p>this is a job portal, online job management system developed by Biruk Metaferia </p>
+                                    
+                                    </div>
+
+                                </div>
+                                
+                                <div class="col-sm-6 col-md-5 mt-30-xs">
+                                    <h5 class="footer-title">Quick Links</h5>
+                                    <ul class="footer-menu clearfix">
+                                        <li><a href="./">Home</a></li>
+                                        <li><a href="AboutUs.php">About Us</a></li>
+                                        <li><a href="Employer.php">Employer</a></li>
+                                        <li><a href="JobSeeker.php">Job Seeker</a></li>
+                                        <li><a href="News.php">Latest News</a></li>
+                                        <li><a href="ContactUs.php">Contact Us</a></li>
+                                        <li><a href="#">Go to top</a></li>
+
+                                    </ul>
+                                
+                                </div>
+
+                            </div>
+
+                        </div>
+                        
+                        <div class="col-sm-12 col-md-3 mt-30-sm">
+                        
+                            <h5 class="footer-title">job portal  Contact</h5>
+                        
+                            <p>Email : <a href="birukm555@gmail.com">birukm555@gmail.com</a></p>
+                            <p>Telegram : <a href="@bkans">@bkans</a></p>
+                            <p>Phone : <a href="+251923160066">+251923160066</a></p>
+                            
+
+                        </div>
+
+                        
+                    </div>
+                    
+                </div>
+                
+            </div>
+            
+            <div class="bottom-footer">
+            
+                <div class="container">
+                
+                    <div class="row">
+                    
+                        <div class="col-sm-4 col-md-4">
+                
+                            <p class="copy-right">&#169; Copyright <?php echo date('Y'); ?> Biruk Metaferia</p>
+                            
+                        </div>
+                        
+                        <div class="col-sm-4 col-md-4">
+                        
+                            <ul class="bottom-footer-menu">
+                                <li  ><a class="developer" href="developer/templates/about.html" >Developed by Biruk Metaferia</a></li>
+                            </ul>
+                        
+                        </div>
+                        
+                    
+                    
+                    </div>
+
+                </div>
+                
+            </div>
+        
+        </footer>
